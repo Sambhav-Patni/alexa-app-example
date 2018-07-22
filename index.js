059f224640bbcfcec879e395920f23ae935afd06
Source Of Arising Mentality <sambhav181@gmail.com>
2018-07-23 00:25:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ alexaApp.intent("nameIntent", {
     ]
   },
   function(request, response) {
-    response.say("Success!, intent invoked "+request.intent.slots.NAME.value);
+    response.say("Success!, intent invoked "+request.slot("NAME"));
   }
 );
 
